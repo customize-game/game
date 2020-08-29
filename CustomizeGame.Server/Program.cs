8bfe4e91b5bab1fb956485b5d0e82e1ec08c51d4
Shass Beleth <shassbeleth@gmail.com>
2020-08-29 23:17:15 +0900

    Feature/magic onion (#4)
    * create editor and scripts folder
    
    * add Google Protobuf
    
    * add system library
    
    * add magic onion library and sample
    
    * add magic onion server sample

---
+++ CustomizeGame.Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using MagicOnion.Hosting;
+using MagicOnion.Server;
+using Microsoft.Extensions.Hosting;
+
+namespace CustomizeGame.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+            await MagicOnionHost.CreateDefaultBuilder()
+                .UseMagicOnion(
+                    new MagicOnionOptions(isReturnExceptionStackTraceInErrorDetail: true),
+                    new ServerPort("0.0.0.0", 12345, ServerCredentials.Insecure))
+                .RunConsoleAsync();
+        }
+    }
+}
---
+++ CustomizeGame/Assets/Scripts/ServerShared/Hubs/SampleHub.cs
@@ -0,0 +1,53 @@
+﻿using MagicOnion;
+using CustomizeGame.ServerShared.MessagePackObjects;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomizeGame.ServerShared.Hubs
+{
+    /// <summary>
+    /// Client -> ServerのAPI
+    /// </summary>
+    public interface ISampleHub : IStreamingHub<ISampleHub, ISampleHubReceiver>
+    {
+        /// <summary>
+        /// ゲームに接続することをサーバに伝える
+        /// </summary>
+        Task JoinAsync(Player player);
+        /// <summary>
+        /// ゲームから切断することをサーバに伝える
+        /// </summary>
+        Task LeaveAsync();
+        /// <summary>
+        /// メッセージをサーバに伝える
+        /// </summary>
+        Task SendMessageAsync(string message);
+        /// <summary>
+        /// 移動したことをサーバに伝える
+        /// </summary>
+        Task MovePositionAsync(Vector3 position);
+    }
+
+    /// <summary>
+    /// Server -> ClientのAPI
+    /// </summary>
+    public interface ISampleHubReceiver
+    {
+        /// <summary>
+        /// 誰かがゲームに接続したことをクライアントに伝える
+        /// </summary>
+        void OnJoin(string name);
+        /// <summary>
+        /// 誰かがゲームから切断したことをクライアントに伝える
+        /// </summary>
+        void OnLeave(string name);
+        /// <summary>
+        /// 誰かが発言した事をクライアントに伝える
+        /// </summary>
+        void OnSendMessage(string name, string message);
+        /// <summary>
+        /// 誰かが移動した事をクライアントに伝える
+        /// </summary>
+        void OnMovePosition(Player player);
+    }
+}
