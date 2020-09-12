c4283ffb94cbfaefc2611e716cfa1eefa80c9616
shita32 <67694633+shita32@users.noreply.github.com>
2020-09-13 01:16:25 +0900

    Add MenuSceneSample&UniRxSample (#7)
    Add MenuSceneSample&UniRxSample
    
    〇Unityのメニューシーンのサンプルを作成
     ・UniRxを使ってイベントお試し
     ・MagicOnionで通信
     ・UI作ってみて使えるもの確認
     ・API検証（Get,Post）
    
    〇以下更新Srcメイン
     ・NetworkRoomManager.cs
     ・ChatManager.cs
     ⇒MagicOnion動作確認とUniRxのイベント
     ・RestAPITest.cs
     ⇒API発行テスト、MagicOnionのサービスでも似たようなことはできる
     ・MenuManager.cs
     ⇒ここに書くつもりだったものをNetworkRoomManager.csに書いてしまった
    
    〇他
     ・サンプルの四角を組み合わせたロボをおまけで同梱
    
    〇サーバー側
     ・GameHub.cs
     ⇒クライアントからイベント発行されたときに実行する処理系
    　ブロードキャストやルーム管理など

---
+++ CustomizeGame/Assets/Scripts/ServerShared/MessagePackObjects/TransformMPO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MessagePack;
+
+namespace CustomizeGame.ServerShared.MessagePackObjects
+{
+    [MessagePackObject]
+    public class TransformMPO
+    {
+        [Key(0)]
+        public Vector3 Position { get; set; }
+
+        [Key(1)]
+        public Quaternion Rotation { get; set; }
+    }
+}
